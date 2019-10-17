73c663bbe41d174d935902d9ab8e9ba15c4ac34d
Tony Eastwood <erlinmaquer@gmail.com>
2019-10-17 19:46:03 +0300

    [ChatManager] Added some methods: createNewChat(), sendMessage(), receiveMessage(), isValid(), saveMyChatPrivateKey(), unloadMyChatPrivateKey().

--- headers/managers/chat_manager.cpp
+++ headers/managers/chat_manager.cpp
@@ -3,7 +3,20 @@
 ChatManager::ChatManager(QByteArray chatId)
 {
     this->_chatId = chatId;
-    this->_encryptionKey = loadChatKey();
+    this->_encryptionKey = unloadChatKey();
+}
+
+bool ChatManager::createNewChat()
+{
+    if (isValid(_chatId))
+    {
+        QByteArray privateChatKey = generateChatKey();
+        saveMyChatPrivateKey(privateChatKey);
+        // send message in blockhain about chat creation
+        return true;
+    }
+    qDebug() << "[Error] createNewChat. Chat id is not valid";
+    return false;
 }
 
 void ChatManager::saveChatKey(QByteArray key)
@@ -18,7 +31,30 @@ void ChatManager::saveChatKey(QByteArray key)
     qDebug() << "[Error] Chat manager can't open file to save the key";
 }
 
-QByteArray ChatManager::loadChatKey()
+void ChatManager::saveMyChatPrivateKey(QByteArray key)
+{
+    QFile file(myChatKeys + this->_chatId);
+    if (file.open(QIODevice::WriteOnly))
+    {
+        file.write(key);
+        file.close();
+        return;
+    }
+    qDebug() << "[Error] Chat manager can't open file to save my key";
+}
+
+QByteArray ChatManager::unloadMyChatPrivateKey()
+{
+    QFile file(myChatKeys + this->_chatId);
+    if (!file.exists())
+        return "0";
+    if (file.open(QIODevice::ReadOnly))
+        return file.readLine();
+    qDebug() << "[Error] Chat manager can't open file to load my key";
+    return "0";
+}
+
+QByteArray ChatManager::unloadChatKey()
 {
     QFile file(chatStore + this->_chatId);
     if (!file.exists())
@@ -31,10 +67,39 @@ QByteArray ChatManager::loadChatKey()
 
 void ChatManager::addMemberToChat(BigNumber actorId)
 {
+    if (unloadMyChatPrivateKey() != "0")
+    {
+        // QByteArray
+        // memberKey=ActorIndex->GetActorById(actorId)->getPublicKey().encrypt(unloadMyChatPrivateKey());
+        // send in blockhaind member key
+    }
 }
 
 void ChatManager::removeMemberFromChat(BigNumber actorId)
 {
+    // regenerate key and create new session
+}
+
+void ChatManager::sendMessage(QByteArray message)
+{
+    // XOR message with _encryptionKey and send it to blockchain
+}
+
+QByteArray ChatManager::receiveMessage(QByteArray message)
+{
+    // XOR message with _encryptionKey and get it
+    return message;
+}
+
+QByteArray ChatManager::generateChatKey()
+{
+    return Utils::calcKeccak(BigNumber::random(64).toByteArray());
+}
+
+bool ChatManager::isValid(BigNumber chatId)
+{
+    // if chat id is accessible for create chat with that id, then return true
+    return true;
 }
 
 QByteArray ChatManager::encryptMessage(QByteArray message)
--- headers/managers/chat_manager.h
+++ headers/managers/chat_manager.h
@@ -1,22 +1,33 @@
 #ifndef CHAT_MANAGER_H
 #define CHAT_MANAGER_H
 #define chatStore "pathToChatStore/"
+#define myChatKeys "pathToMyChatKey/" // should be outside the blockchain
 #include "QByteArray"
 #include <QFile>
 #include <QDebug>
-#include "utils/bignumber.h"
+//#include "utils/bignumber.h"
+#include "utils/utils.h"
 class ChatManager
 {
 private:
     QByteArray _chatId = "0";
     QByteArray _encryptionKey = "0";
 
+private:
+    QByteArray generateChatKey();
+    bool isValid(BigNumber chatId); // if chat id is not occupied
+    void saveMyChatPrivateKey(QByteArray key);
+    QByteArray unloadMyChatPrivateKey();
+
 public:
     ChatManager(QByteArray chatId);
+    bool createNewChat();
     void saveChatKey(QByteArray key);
-    QByteArray loadChatKey();
+    QByteArray unloadChatKey();
     void addMemberToChat(BigNumber actorId);
     void removeMemberFromChat(BigNumber actorId);
+    void sendMessage(QByteArray message);
+    QByteArray receiveMessage(QByteArray message);
     QByteArray encryptMessage(QByteArray message);
     QByteArray decryptMessage(QByteArray message);
     ~ChatManager();
