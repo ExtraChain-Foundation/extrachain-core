46c18ff6bd0cf9398e3c3a8736695cbc5ab19a53
Draqonic <dranoq@zoho.eu>
2019-10-25 19:25:01 +0300

    [Logs] Fix file filters for logs model

--- sources/managers/logs_manager.cpp
+++ sources/managers/logs_manager.cpp
@@ -77,6 +77,9 @@ void LogsManager::makeLog(const QString& file, int line, const QString& function
         }
     }
 
+    if (!isPrint)
+        return;
+
     QString fileNameQrc, lineRow;
     if (fileName.right(3) == "qml")
     {
