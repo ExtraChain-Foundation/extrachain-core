5848896639641e8d2efcb6442e1a8f71dfa4a4d3
Vladyslav Halasiuk <nsgi.ngame@gmail.com>
2019-11-29 15:11:22 +0200

    [WIP]

--- dfs/managers/headers/card_manager.h
+++ dfs/managers/headers/card_manager.h
@@ -7,16 +7,16 @@
 class CardManager
 {
 public:
-    static QStringList getAll(based_dfs_struct::Type type);
-    static QStringList getFilesByType(const QString &userId, based_dfs_struct::Type type);
+    static QStringList getAll(dfsStruct::Type type);
+    static QStringList getFilesByType(const QString &userId, dfsStruct::Type type);
     static QByteArray getLastFileName(const QString &userId);
     static QStringList getAllFiles(const QByteArray &userId);
-    static based_dfs_struct::Type getTypeByName(const QString &path, const QByteArray &userId);
+    static dfsStruct::Type getTypeByName(const QString &path, const QByteArray &userId);
 
-    static QString buildPathForFile(const QString &userId, const QString &file, based_dfs_struct::Type type,
+    static QString buildPathForFile(const QString &userId, const QString &file, dfsStruct::Type type,
                                     bool localFormat);
     static QStringList buildPathForFiles(const QString &userId, const QStringList &files,
-                                         based_dfs_struct::Type type, bool localFormat);
+                                         dfsStruct::Type type, bool localFormat);
 
 private:
     CardManager() = default;
