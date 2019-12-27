02792db81350368734910de10316a1a3dc320460
Draqonic <dranoq@zoho.eu>
2019-12-27 22:47:53 +0200

    [temp]

--- sources/network/socket_service.cpp
+++ sources/network/socket_service.cpp
@@ -130,7 +130,7 @@ void SocketService::sendMsg(const QByteArray &data, const SocketPair &socketData
 void *SocketService::distMsg(const QByteArray data, const SocketPair socketData)
 {
     emit msgReady(data, socketData);
-    QCoreApplication::processEvents();
+    //QCoreApplication::processEvents();
     return nullptr;
 }
 
