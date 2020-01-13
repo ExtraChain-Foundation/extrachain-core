5d1bae62289d081eea2e462309ca342432b833c3
Vladyslav Halasiuk <nsgi.ngame@gmail.com>
2020-01-13 18:31:42 +0200

    [WIP]

--- dfs/managers/sources/sender.cpp
+++ dfs/managers/sources/sender.cpp
@@ -50,7 +50,7 @@ void Sender::sendFragments(QString path, DfsStruct::Type type, QByteArray frag, 
             pck.dataHash = title.dataHash;
             pck.pckgNumber = fragsID[i];
             pck.data = data;
-            QThread::currentThread()->sleep(1);
+            //            QThread::currentThread()->wait(1000);
             sendDfsMessage(pck, Messages::DFSMessage::fileDataMessage, receiver);
         }
     }
