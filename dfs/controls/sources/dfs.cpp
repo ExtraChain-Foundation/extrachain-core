ac0eb0d66b13986f56a2ee2281e73b91b3369a94
Draqonic <dranoq@zoho.eu>
2020-01-31 19:48:01 +0200

    [DFS]

--- dfs/controls/sources/dfs.cpp
+++ dfs/controls/sources/dfs.cpp
@@ -466,7 +466,10 @@ void Dfs::saveFN(const QString tmpPath, const QString &path, const DfsStruct::Ty
             cardFileChange.sign = itemFuture[0]["sign"].c_str();
             sender->sendDfsMessage(cardFileChange, Messages::DFSMessage::cardFileChange);
         }
-        rootFuture.close();
+        else
+        {
+            rootFuture.close();
+        }
     }
 
     this->dfsValidate(userId);
