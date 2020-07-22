a6e94957571d95010fb03048a5b3173e981db4ce
Vladyslav Halasiuk <vgalasyuk0@gmail.com>
2020-07-23 01:25:20 +0300

    [fix] const

--- sources/utils/exc_utils.cpp
+++ sources/utils/exc_utils.cpp
@@ -571,13 +571,13 @@ std::string Utils::byteToHexString(std::vector<unsigned char> &data)
     return s;
 }
 
-std::string Utils::byteToHexString(std::string data)
+std::string Utils::byteToHexString(const std::string &data)
 {
     std::vector<unsigned char> v(data.begin(), data.end());
     return byteToHexString(v);
 }
 
-std::string Utils::hexStringToByte(std::string data)
+std::string Utils::hexStringToByte(const std::string &data)
 {
     std::vector<unsigned char> p;
     p.resize(data.length() / 2 + 1);
