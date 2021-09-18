536cbea4a57112679550be45b83b5b56f31e0631
Draqonic <dranoq@zoho.eu>
2021-09-19 00:25:40 +0300

    [Socket] Remove actor

--- headers/network/isocket_service.h
+++ headers/network/isocket_service.h
@@ -2,10 +2,8 @@
 #define ISOCKETSERVICE_H
 
 #include "utils/exc_utils.h"
-#include "datastorage/actor.h"
 
 class NetworkManager;
-class ActorIndex;
 
 class EXTRACHAIN_EXPORT SocketService : public QObject
 {
@@ -50,8 +48,8 @@ protected:
     int m_bytesCompressed = 0;
 
 private:
-    Actor<KeyPrivate> priv;
-    std::string pub;
+    QByteArray priv;
+    QByteArray pub;
 };
 
 #endif // WEBSOCKETSERVICE_H
