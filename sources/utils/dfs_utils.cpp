d504966a2909b7eabde09ad96b3ead3af4a04689
Draqonic <dranoq@zoho.eu>
2022-03-28 23:24:41 +0300

    [DFS] Fix typos in sql

--- sources/utils/dfs_utils.cpp
+++ sources/utils/dfs_utils.cpp
@@ -1,13 +1,13 @@
 #include "utils/dfs_utils.h"
 
 std::vector<DBRow> DFS::Tables::ActorDirFile::getFileDataByHash(DBConnector *db, std::string hash) {
-    std::string query = "SELECT * FROM " + TableName + "WHERE fileHash = '" + hash + "' "
-        + "OR fileHashPrev = " + hash + "' ";
+    std::string query = "SELECT * FROM " + TableName + " WHERE fileHash = '" + hash + "' "
+        + "OR fileHashPrev = '" + hash + "' ";
     return db->select(query);
 }
 
 std::vector<DBRow> DFS::Tables::LocalDirFile::getFileDataByHash(DBConnector *db, std::string hash) {
-    std::string query = "SELECT * FROM " + TableName + "WHERE fileHash = '" + hash + "' "
-            + "OR fileHashPrev = " + hash + "' ";
+    std::string query = "SELECT * FROM " + TableName + " WHERE fileHash = '" + hash + "' "
+        + "OR fileHashPrev = '" + hash + "' ";
     return db->select(query);
 }
