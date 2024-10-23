fcb3f25183eda4477d26ed4e38f2394abd09c765
Serhii <sereganyamtsu@gmail.com>
2024-10-23 19:39:28 +0300

    Update cmakelist

--- headers/network/message_body.h
+++ headers/network/message_body.h
@@ -55,11 +55,12 @@ enum class MessageType {
     ShareConnections = 113,
     Accrual = 120,
 
+    Unknown = 404,
+
     VPNHandshake = 1001,
     VPNConnection = 1002,
     VPNDisconnect = 1003,
     VPNUpdateConnection = 1004,
-    Unknown = 404
 };
 MSGPACK_ADD_ENUM(MessageType)
 FORMAT_ENUM(MessageType)
