cc043f86f3d26a5cb1fabd3a6dc63827a9bbdaa5
Draqonic <dranoq@zoho.eu>
2024-11-19 14:18:45 +0100

    Update all licenses, add script for update

--- headers/managers/token_manager.h
+++ headers/managers/token_manager.h
@@ -1,6 +1,6 @@
 /*
  * ExtraChain Core
- * Copyright (C) 2020 ExtraChain Foundation <extrachain@gmail.com>
+ * Copyright (C) 2025 ExtraChain Foundation <official@extrachain.io>
  *
  * This library is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
