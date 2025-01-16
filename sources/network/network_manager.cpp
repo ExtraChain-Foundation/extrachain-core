e8037a4fb646836d8447f9f20f5c7f8a6cc81082
Draqonic <dranoq@zoho.eu>
2025-01-16 14:49:31 +0100

    [Core]

--- sources/network/network_manager.cpp
+++ sources/network/network_manager.cpp
@@ -590,7 +590,7 @@ void NetworkManager::messageReceived(const std::string &message,
     if (sign_actor) {
         if (!sign_actor.value().key().verify(ByteArray(message_body.serializeForSign()).toBytes(),
                                              ByteArray(sign.data()).toArray<crypto_sign_BYTES>())) {
-            eWarning("Sign package is invalid!");
+            // eWarning("Sign package is invalid!");
             // return;
         }
     } else {
