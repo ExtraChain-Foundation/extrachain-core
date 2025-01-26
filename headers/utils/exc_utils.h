ba4632075f0452d8f7c6acc9fd2797ea4ebdf6eb
Draqonic <dranoq@zoho.eu>
2025-01-26 13:55:44 +0100

    Set block creation timer to 5 sec

--- headers/utils/exc_utils.h
+++ headers/utils/exc_utils.h
@@ -309,7 +309,7 @@ namespace Config {
         static const int SECTION_SIZE = 1000;
 
         // How often to construct block from pending transactions (in miliseconds)
-        static const int BLOCK_CREATION_PERIOD = 2000;
+        static const int BLOCK_CREATION_PERIOD = 5000;
 
         // How often to construct genesis block (in blocks)
         static const int CONSTRUCT_GENESIS_EVERY_BLOCKS = 100;
