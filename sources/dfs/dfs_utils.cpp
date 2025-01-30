9f582facff8cf0aa93339897af567beebc456415
Andrii <andreea0008@gmail.com>
2025-01-30 17:53:34 +0200

    [future] filter system files p.2

--- sources/dfs/dfs_utils.cpp
+++ sources/dfs/dfs_utils.cpp
@@ -67,7 +67,10 @@ std::expected<std::vector<Dfs::DirRow>, Dfs::DfsError> Dfs::Tables::ActorDirFile
 
     std::vector<Dfs::DirRow> dirRows;
     auto                     actrDirData =
-        db.select(fmt::format("SELECT * FROM {} WHERE last_modified >= {} NOT LIKE ':%'", TableName, last_modified));
+        db.select(fmt::format("SELECT * FROM {} WHERE last_modified >= {} AND state != '0' AND name NOT LIKE ':%'",
+                              TableName,
+                              last_modified,
+                              std::to_underlying(FileState::Removed)));
 
     for (auto &row : actrDirData) {
         auto dirRow = Utils::from_dbrow<Dfs::DirRow>(row);
