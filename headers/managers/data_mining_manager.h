00199969b5ecf8c2d7f555e60149cefc523d73fb
Draqonic <dranoq@zoho.eu>
2025-02-04 01:20:55 +0100

    [Data Mining] Use 0.05 as reward coefficient

--- headers/managers/data_mining_manager.h
+++ headers/managers/data_mining_manager.h
@@ -33,7 +33,7 @@ class DataMiningManager : public QObject {
 
     ExtraChainNode      *node;
     const int            CoinProductionRate = 100;
-    const BigNumberFloat KoefReward         = BigNumberFloat("0.25", NumeralBase::Dec);
+    const BigNumberFloat KoefReward         = BigNumberFloat("0.05", NumeralBase::Dec);
     bool                 isRecalculate      = false;
 
 public:
