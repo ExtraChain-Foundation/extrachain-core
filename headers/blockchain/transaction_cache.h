ccc9f40bc0ed62d3b239477a07185aa6dc97c1cf
Draqonic <dranoq@zoho.eu>
2025-03-05 16:25:55 +0100

    [TransactionnCache] Add hidden reward

--- headers/blockchain/transaction_cache.h
+++ headers/blockchain/transaction_cache.h
@@ -33,13 +33,13 @@ public:
 
 signals:
     void add(const BigNumber &block_id, uint64_t block_date, const Transaction &transaction);
-    void request(ActorId actor_id, TokenId token, int offset);
+    void request(ActorId actor_id, TokenId token, bool reward_hidden, int offset);
     void response(ActorId actor_id, TokenId token, int offset, std::vector<TransactionInfo> txs);
     void make_cache();
 
 private slots:
     void adding(const BigNumber &block_id, uint64_t block_date, const Transaction &transaction);
-    void prepare(ActorId actor_id, TokenId token, int offset);
+    void prepare(ActorId actor_id, TokenId token, bool reward_hidden, int offset);
     void cache();
 
 private:
