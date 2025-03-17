b5146bc7663dc540b2f8502d915a629688a9843d
Draqonic <dranoq@zoho.eu>
2025-03-17 21:03:03 +0100

    [Dfs] Move from system to main with encryption

--- sources/managers/extrachain_node.cpp
+++ sources/managers/extrachain_node.cpp
@@ -318,6 +318,41 @@ void ExtraChainNode::start() {
 
         emit m_blockchain->transaction_cache().make_cache();
     }
+
+    // Version compatibility: 0.16.1 (temp)
+#ifdef IS_RC
+    std::thread rc_thread([this]() {
+        auto system_id     = m_accountController->system_actor().id();
+        auto main_id       = m_accountController->currentProfile().main_id();
+        auto data_security = Dfs::DataSecuritySelf { .my_actor = main_id };
+
+        auto dir_rows = Dfs::Tables::ActorDirFile::get_dir_rows(system_id);
+        if (!dir_rows.has_value()) {
+            return;
+        }
+
+        for (const auto& row : dir_rows.value()) {
+            auto file_path = Dfs::Path::file_path(system_id, row.file_id);
+            if (!file_path.has_value()) {
+                continue;
+            }
+
+            auto store = m_dfs->store_file(main_id,
+                                           main_id,
+                                           file_path->native(),
+                                           row.folder.has_value() ? row.folder.value() : "",
+                                           row.name,
+                                           Dfs::DataSecurity::Self,
+                                           data_security);
+
+            if (store.has_value()) {
+                m_dfs->remove_stored_file(system_id, row.file_id);
+            }
+        }
+    });
+
+    rc_thread.detach();
+#endif
 }
 
 // void ExtraChainNode::connectResolveManager() {
