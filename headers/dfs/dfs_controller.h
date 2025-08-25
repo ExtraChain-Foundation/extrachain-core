efa2a58de25fe8eeeb8f35711444233c402cc36c
Draqonic <dranoq@zoho.eu>
2025-08-25 20:17:10 +0200

    [Dfs] Add is dirs loaded prop, cleanups

--- headers/dfs/dfs_controller.h
+++ headers/dfs/dfs_controller.h
@@ -177,6 +177,10 @@ public:
         Utils::write_settings(settings);
     }
 
+    bool is_dirs_loaded() {
+        return is_dirs_loaded_;
+    }
+
     std::expected<Dfs::DirRow, Dfs::DfsError> store_file(
         const ActorId               &owner_id,
         const ActorId               &author_id,
@@ -410,6 +414,7 @@ public:
 private:
     DirsManager dirs_manager_;
     LoadManager load_manager_;
+    bool        is_dirs_loaded_ = false;
 
     std::unordered_map<std::string, Dfs::DirRow> files_ready_status_;
     std::set<std::pair<ActorId, std::string>>    files_waiting_;
@@ -442,7 +447,6 @@ public:
     void  sendSizeRequestMsg(const ActorId &actorId) const;
     void  sendSizeReponseMsg(const DfsP::RequestDfsSize &msg, const Responder &responder); // TODO: const
     float percentVerified(std::vector<DfsP::VerifyFileMessage> &fileList);
-    void  loadVPNLocalizationFiles();
 
 public slots:
 
@@ -463,13 +467,13 @@ signals:
     void downloaded(ActorId owner_id, Dfs::DirRow dirRow);
     void downloadProgress(ActorId owner_id, std::string file_id, int progress);
     void waitDownloaded(ActorId owner_id, Dfs::DirRow dirRow);
+    void dirsLoaded();
 
     void collectionDownloaded(); // temp signal for beginFetchNextFile
     void collectionChanged(ActorId owner_id, Dfs::DirRow dir_row, HistoricalCollectionRow historical_row);
     void vectorRowAdded(ActorId owner_id, Dfs::DirRow dir_row, DbRow row);
     void vectorRowRemoved(ActorId owner_id, Dfs::DirRow dir_row, DbRow row);
 
-    //
-    void getRemovedVPNLocalizationInfo(const QString data, const std::string actorId);
-    void vpnLocalizationLoadedFromStorage(const std::string actorId, const std::string fileName);
+    friend DirsManager;
+    friend LoadManager;
 };
