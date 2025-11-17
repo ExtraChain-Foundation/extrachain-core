780947608f8c04f9cba485d629126f6a653a208d
Draqonic <dranoq@zoho.eu>
2025-11-17 21:12:37 +0100

    [Dag]

--- sources/chain/dag_cache.cpp
+++ sources/chain/dag_cache.cpp
@@ -178,7 +178,9 @@ void DagCache::write_cached_balances(const Balances& balances, const std::option
     }
 
     // Lock mutex to protect transaction block from concurrent access
+    // eLog("MUTEX! Start");
     std::unique_lock<std::mutex> lock(mutex_);
+    eLog("MUTEX! Continue");
 
     // Start a transaction for efficiency
     cache_db_->query("BEGIN TRANSACTION");
@@ -210,6 +212,7 @@ void DagCache::write_cached_balances(const Balances& balances, const std::option
     }
 
     eLog("[DagCache] Wrote {} balances to cache", balances.size());
+    // eLog("MUTEX! End");
 }
 
 BigNumberFloat DagCache::read_cached_balance(const ActorId& actor_id, const TokenId& token_id) {
@@ -453,23 +456,23 @@ void DagCache::check_and_update_cache_thread(const SectionId& current_section) {
                 return;
             }
 
-            ThreadPoolBoost::instance()->post([this, res] {
-                auto last_hash    = node->dag()->generate_hash_from_section(res.from);
-                auto control_hash = node->dag()->read_control(res.to);
-                if (!control_hash.has_value()) {
-                    eCritical("[DagCache] Problem with control hash from {}", res.to);
-                    return;
-                }
-                if (!last_hash.has_value()) {
-                    eCritical("[DagCache] No last hash");
-                    return;
-                }
+            // ThreadPoolBoost::instance()->post([this, res] {
+            auto last_hash    = node->dag()->generate_hash_from_section(res.from);
+            auto control_hash = node->dag()->read_control(res.to);
+            if (!control_hash.has_value()) {
+                eCritical("[DagCache] Problem with control hash from {}", res.to);
+                return;
+            }
+            if (!last_hash.has_value()) {
+                eCritical("[DagCache] No last hash");
+                return;
+            }
 
-                auto hash_interval = HashInterval { .from = res.from, .to = res.to, .hash = last_hash.value() };
-                eLog("[Dag] Cache from {} to {}", res.from.to_int(), res.to.to_int());
-                // eLog("[Dag] Send {}", hash_interval);
-                node->network()->send_message(hash_interval, MessageType::DagIntervalHash, SendMode::Neighbours);
-            });
+            auto hash_interval = HashInterval { .from = res.from, .to = res.to, .hash = last_hash.value() };
+            eLog("[Dag] Cache from {} to {}", res.from.to_int(), res.to.to_int());
+            // eLog("[Dag] Send {}", hash_interval);
+            node->network()->send_message(hash_interval, MessageType::DagIntervalHash, SendMode::Neighbours);
+            // });
         }
     }
 }
@@ -491,7 +494,9 @@ std::pair<bool, SectionId> DagCache::update_to_genesis_section(
     }
 
     // Lock mutex to protect transaction block from concurrent access
+    // eLog("MUTEX! Start");
     std::unique_lock<std::mutex> lock(mutex_);
+    // eLog("MUTEX! Continue");
 
     bool show = dag->status_ == DagStatus::Sync ? genesis_section % 500 == 0 : true;
     if (show) {
@@ -596,6 +601,7 @@ std::pair<bool, SectionId> DagCache::update_to_genesis_section(
     set_section(genesis_section);
     // eLog("[DagCache] Cache updated to section {}", cached_section_);
     dag->update_range();
+    // eLog("MUTEX! End");
     return { true, start_section };
 }
 
@@ -652,15 +658,19 @@ void DagCache::process_transaction(const Transaction& tx, Balances& balances) {
 }
 
 bool DagCache::init_db() {
-    std::unique_lock<std::mutex> lock(mutex_);
     if (db_initialized_) {
         return true;
     }
 
     if (cache_db_ && cache_db_->is_open()) {
         db_initialized_ = true;
+
         return true;
     }
+
+    // eLog("MUTEX! Start");
+    std::unique_lock<std::mutex> lock(mutex_);
+    // eLog("MUTEX! Continue");
 
     // bool is_exists = QFile(QString::fromStdString(ChainConst::BALANCE_CACHE)).exists();
 
@@ -672,6 +682,8 @@ bool DagCache::init_db() {
 
     if (!cache_db_->open()) {
         eLog("[DagCache] Failed to open cache database");
+
+        // eLog("MUTEX! End");
         return false;
     }
 
@@ -680,6 +692,8 @@ bool DagCache::init_db() {
 
     if (!success) {
         eLog("[DagCache] Failed to create cache table");
+
+        // eLog("MUTEX! End");
         return false;
     }
 
@@ -700,6 +714,8 @@ bool DagCache::init_db() {
 
     eLog("[DagCache] Cache database initialized");
     db_initialized_ = true;
+
+    // eLog("MUTEX! End");
     return true;
 }
 
