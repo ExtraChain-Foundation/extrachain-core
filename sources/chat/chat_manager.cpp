0ad4518efdb0ebff560954d2a1c5d91fac63adbf
Andrii <andreea0008@gmail.com>
2025-12-10 11:32:39 +0200

    [fix] init message data type on add_new_message_text

--- sources/chat/chat_manager.cpp
+++ sources/chat/chat_manager.cpp
@@ -418,6 +418,7 @@ std::expected<bool, ChatError> ChatManager::add_new_message_text(const ActorId& 
     // ... checks for file ...
 
     auto message_data = Chat::MessageData { .data = message_text.text, .reply_id = message_text.reply_id };
+    message_data.type = Chat::MessageType::Text;
     // auto message_data_json = Json::serialize(message_data);
     auto message = Chat::Message { .id = Utils::generate_random_hex(6), .message = message_data };
     // TODO: with id exists check
