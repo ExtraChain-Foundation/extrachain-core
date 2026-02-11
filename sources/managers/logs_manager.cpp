18fb2a0bbbf234f5b211a4faeddce2e0a6945ac7
mmvrsl <mamaiev.rsln@gmail.com>
2026-02-11 03:57:56 +0200

    WIN32_LEAN_AND_MEAN flag

--- sources/managers/logs_manager.cpp
+++ sources/managers/logs_manager.cpp
@@ -18,6 +18,11 @@
  */
 
 #ifdef _WIN32
+    #ifndef WIN32_LEAN_AND_MEAN
+        #define WIN32_LEAN_AND_MEAN
+    #endif
+    #include <winsock2.h>
+    #include <ws2tcpip.h>
     #include <Windows.h>
 #endif
 
